78e48470866b345c146618ed6e58040bfb9a57a5
Sejal Chaudhary <sejalchaudhary0708@gmail.com>
2026-09-07 13:47:44 +0530

    Time: 3 ms (67.32%), Space: 10.7 MB (93.37%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -9,7 +9,7 @@ public:
         while(r<s.size()){
             hash[s[r]-'A']++;
             maxf  = max(maxf , hash[s[r]-'A']);
-            while((r-l+1)-maxf > k){
+            if((r-l+1)-maxf > k){
                 hash[s[l]-'A']--;
                 maxf = 0;
                 for(int i=0;i<26;i++){
