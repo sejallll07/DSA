b179f0ca5a2cbe1ca6bf5f5c3d812b21a700612f
Sejal Chaudhary <sejalchaudhary0708@gmail.com>
2026-09-10 00:10:26 +0530

    Time: 0 ms (100%), Space: 9 MB (57.33%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long comma = 0;
+
+        for (long long x = 1000; x <= n; x *= 1000) {
+            comma += n - x + 1;
+        }
+
+        return comma;
+    }
+};
